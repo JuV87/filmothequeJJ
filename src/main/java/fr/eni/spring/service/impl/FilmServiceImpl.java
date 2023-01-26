add4792761db2a35b335f274c896918d2d6ec355
ESHolyblast <jolan.vandenbroucque@outlook.fr>
2023-01-26 15:17:59 +0100

    ajout BS

--- src/main/java/fr/eni/spring/service/impl/FilmServiceImpl.java
+++ src/main/java/fr/eni/spring/service/impl/FilmServiceImpl.java
@@ -13,6 +13,8 @@ public class FilmServiceImpl implements FilmService {
     private List<Film> listFilm = new ArrayList<Film>();
 
     public FilmServiceImpl() {
+
+
         Film film = new Film(0, "Harry Pouter", 2001, 140, "Je suis un synopsis");
         Film film2 = new Film(1, "Le seigneur des anneaux", 2003, 300, "Je suis un synopsis intéressant");
 
@@ -23,7 +25,13 @@ public class FilmServiceImpl implements FilmService {
     @Override
     public Film showMovie(long id) {
         // TODO Boucle for pour retrouver le film avec l'id
-        return listFilm.get((int) id);
+
+        for(Film f: listFilm) {
+            if(f.getId()==id){
+                return f;
+            }
+        }
+        return null;
     }
 
     @Override
