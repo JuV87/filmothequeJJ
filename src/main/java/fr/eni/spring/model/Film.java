2218487d4403ae86e76f106e9c413dc189d8c3ff
JuV87 <99645371+JuV87@users.noreply.github.com>
2023-01-26 16:12:42 +0100

    styling css

--- src/main/java/fr/eni/spring/model/Film.java
+++ src/main/java/fr/eni/spring/model/Film.java
@@ -5,23 +5,23 @@ import lombok.Data;
 @Data
 public class Film {
 
-    public Film(long id, String titre, int annee, int duree, String synopsis) {
-        this.id = id;
-        this.titre = titre;
-        this.annee = annee;
-        this.duree = duree;
-        this.synopsis = synopsis;
-    }
-    
-    public Film(long id) {
-        this.id = id;
+	public Film(long id, String titre, int annee, int duree, String synopsis) {
+		this.id = id;
+		this.titre = titre;
+		this.annee = annee;
+		this.duree = duree;
+		this.synopsis = synopsis;
+	}
 
-    }
-    
+	public Film(long id) {
+		this.id = id;
+
+	}
 
 	public long id;
-    public String titre;
-    public int annee;
-    public int duree;
-    public String synopsis;
+	public String titre;
+	public int annee;
+	public int duree;
+
+	public String synopsis;
 }
