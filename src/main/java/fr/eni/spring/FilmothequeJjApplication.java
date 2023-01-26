448fbfb140d40e097b903318bffe8a48aff85ef8
JuV87 <99645371+JuV87@users.noreply.github.com>
2023-01-26 09:18:23 +0100

    Initial commit

---
+++ src/main/java/fr/eni/spring/FilmothequeJjApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmothequeJjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmothequeJjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/spring/FilmothequeJjApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmothequeJjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
